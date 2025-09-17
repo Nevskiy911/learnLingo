de349c1a924f111f9df409b3cf71bf454035ed20
Nevskiy911 <maleev1820@gmail.com>
2025-09-17 21:02:27 +0200

    my first steps in tw

---
+++ my-app/src/pages/Favorites.jsx
@@ -0,0 +1,9 @@
+function Favorites() {
+  return (
+    <div>
+      <h2>Favorites page</h2>
+    </div>
+  );
+}
+
+export default Favorites;
---
+++ my-app/src/pages/Teatchers.jsx
@@ -0,0 +1,9 @@
+function Teachers() {
+  return (
+    <div>
+      <h2>Teachers page</h2>
+    </div>
+  );
+}
+
+export default Teachers;
